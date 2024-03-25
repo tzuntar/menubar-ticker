dcf304fd8f41448988192a414f195ad1316d7455
Tobija Žuntar <tobija.zuntar@gmail.com>
2024-03-25 13:41:33 +0100

    Restore iTunes support and adapt it to work with iTunes installed on macOS versions above Mojave using Retroactive

--- MenubarTicker/AppDelegate.m
+++ MenubarTicker/AppDelegate.m
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import "Music.h"
+#import "iTunes.h"
 #import "Spotify.h"
 
 const NSTimeInterval kPollingInterval = 10.0;
@@ -9,6 +10,7 @@ const NSTimeInterval kPollingInterval = 10.0;
 @interface AppDelegate ()
 
 @property (nonatomic, retain) MusicApplication *music;
+@property (nonatomic, retain) iTunesApplication *iTunes;
 @property (nonatomic, retain) SpotifyApplication *spotify;
 
 @property (nonatomic, retain) NSStatusItem *statusItem;
@@ -31,6 +33,7 @@ const NSTimeInterval kPollingInterval = 10.0;
     [[NSDistributedNotificationCenter defaultCenter] removeObserver:self name:nil object:nil];
 
     self.music = nil;
+    self.iTunes = nil;
     self.spotify = nil;
     
     self.statusItem = nil;
@@ -71,6 +74,13 @@ const NSTimeInterval kPollingInterval = 10.0;
 - (void)awakeFromNib
 {
     self.music = [SBApplication applicationWithBundleIdentifier:@"com.apple.music"];
+
+    // Doesn't work in > Mojave with manually installed iTunes
+    //self.iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+    NSArray<NSRunningApplication *> *runningApps = [NSRunningApplication
+                                                    runningApplicationsWithBundleIdentifier:@"com.apple.iTunes"];
+    self.iTunes = [SBApplication applicationWithProcessIdentifier:runningApps[0].processIdentifier];
+
     self.spotify = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
     
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
@@ -85,7 +95,9 @@ const NSTimeInterval kPollingInterval = 10.0;
 {
     id currentTrack = nil;
     
-    if ([self.music isRunning] && [self.music playerState] == MusicEPlSPlaying) {
+    if ([self.iTunes isRunning] && [self.iTunes playerState] == iTunesEPlSPlaying) {
+        currentTrack = [self.iTunes currentTrack];
+    } else if ([self.music isRunning] && [self.music playerState] == MusicEPlSPlaying) {
         currentTrack = [self.music currentTrack];
     } else if ([self.spotify isRunning] && [self.spotify playerState] == SpotifyEPlSPlaying) {
         currentTrack = [self.spotify currentTrack];
